40d0c877ecc26dc42fad31038453bdfc2f613bb0
Pongtawan Klysubun <pongtawan.klysubun@gmail.com>
2026-03-11 17:56:48 +0700

    Initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+export default function ProductCard({venueName, imgSrc}:{venueName:string,imgSrc:string}) {
+    return(
+        <div className='w-1/5 h-[300px] rounded-lg shadow-lg'>
+            <div className='w-full h-[70%] relative rounded-t-lg'>
+                <Image src={imgSrc}
+                alt = 'Product Picture'
+                fill={true}
+                className='object-cover rounded-t-lg'
+                />
+            </div>
+            <div className='w-full h-[30%] p-[10px]'>{venueName}</div>
+        </div>
+    );
+}
